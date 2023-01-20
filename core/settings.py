d7e0452a508bb19f707033271546afa178a45da0
Wanderson Duarte <wanderson.barbosa.duarte24@gmail.com>
2023-01-20 10:16:38 -0300

    commit

--- core/settings.py
+++ core/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/arquivo/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'arquivo')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- desafio/views.py
+++ desafio/views.py
@@ -78,9 +78,9 @@ class MulheresDeMereenViewSet(viewsets.ModelViewSet):
 class ListaDePessoasViewSet(viewsets.ModelViewSet):
     queryset = DadosArquivo.objects.all()
     serializer_class = ListaDePessoasSerializer
-
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    #TODO: README
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['nome', 'sobrenome']
     ordering_fields = ['nascimento']
-    #search_fields = ['nome']
     filterset_fields = ['sexo']
     
